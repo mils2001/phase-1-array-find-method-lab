f9c4b3764f4a4810ee89b5772318421bd0115299
chris <Mileschris466@gmail.com>
2024-10-04 01:34:53 +0300

    find

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+
+
+const record = [
+    { year: "2018", result: "N/A" },
+    { year: "2017", result: "N/A" },
+    { year: "2016", result: "N/A" },
+    { year: "2015", result: "W" },  
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+];
+
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+}
+
+describe('superbowlWin(record)', function () {
+    it('returns the year the Denver Broncos won the Super Bowl', function () {
+        expect(superbowlWin(record)).to.equal("2015");  
+    });
+
+    it('returns undefined if there is no win', function () {
+        const sadReality = [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ];
+        expect(superbowlWin(sadReality)).to.equal(undefined);  
+    });
+});
+
+
